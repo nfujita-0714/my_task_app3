da7e17af150a87a28740a5cbbc775f490df95e58
nfujita0707 <nfujita0714@gmail.com>
2021-09-13 05:57:43 +0000

    test fin

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
     title { 'test_title' }
     contents { 'test_content' }
   end
+  factory :second_task, class: Task do
+    # 下記の内容は実際に作成するカラム名に合わせて変更してください
+    title { 'test_title2' }
+    contents { 'test_content2' }
+  end
 end
