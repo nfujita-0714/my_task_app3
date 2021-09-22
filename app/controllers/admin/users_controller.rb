e15735c54683bf1ecd7dcb62faa4af3ceb68dc2a
nfujita0707 <nfujita0714@gmail.com>
2021-09-22 07:29:10 +0000

    ログイン機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,53 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-  end
+  before_action :admin_check
+  before_action :set_user, only: %i[ show edit update destroy]
 
   def index
+    @users = User.all.includes(:tasks)
+  end
+  def show
+    @tasks = @user.tasks
+  end
+  def new
+    @user = User.new
+  end
+  def edit
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice:"ユーザーを登録しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "ユーザーを更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザーを削除しました"
+    else
+      redirect_to admin_users_path, notice: "管理者がいなくなるので削除できません"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def admin_check
+    unless current_user && current_user.admin?
+      redirect_to root_path, notice: "管理者以外はアクセスできません"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
   end
   root "tasks#index"
   resources :tasks
+  namespace :admin do
+    resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
