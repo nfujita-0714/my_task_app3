488c9398ef456b9b3b89672b4b25c0324fd13846
nfujita0707 <nfujita0714@gmail.com>
2021-09-24 01:29:21 +0000

    seedデータ、テスト再実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@
   password = "password"
   User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
+
+1.times do |n|
+  name = Faker::JapaneseMedia::Naruto.character
+  email = Faker::Internet.email
+  password = "password"
+  password_confirmation = "password"
+  User.create!(name: name,email: email,password: password,password_confirmation: password_confirmation,admin: true)
+end
