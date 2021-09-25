f221d73ae1b00c4075241f3e264972142c6be19b
nfujita0714 <nfujita0714@gmail.com>
2021-09-25 09:40:05 +0000

    Labelモデル、Labelingモデル作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,7 @@ class Task < ApplicationRecord
   enum priority: {高:0, 中:1, 低:2}
   scope :search_title, -> (title) { where("title LIKE ?", "%#{title}%") }
   scope :search_status, -> (status) { where(status: status)}
+
+  has_many :labelings, dependent: :destroy
+  has_many :labeling_labels, through: :labelings, source: :label
 end
