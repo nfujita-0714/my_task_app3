fe8697e5d99ca66219374cb5d1a426f1a04b89d7
nfujita0714 <nfujita0714@gmail.com>
2021-09-25 02:37:29 +0000

    エラー対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  def admin_exist_check
+    throw :abort if self.admin? && User.where(admin: true).count == 1
+  end
+  def admin_exist_check_update
+    @admin_user = User.where(admin: true)
+    throw :abort if @admin_user.first == self && @admin_user.count == 1
+  end
+  before_destroy :admin_exist_check
+  before_update :admin_exist_check_update
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }
   validates :email, uniqueness: true
